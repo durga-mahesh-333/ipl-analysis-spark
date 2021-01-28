17996fe60441cc339ba50e549287ef4dfe7d171c
durgaMahesh333 <67140575+durgaMahesh333@users.noreply.github.com>
2021-01-28 11:13:01 +0530

    small changes

--- src/battingAnalysis/battingAverage/BattingAverage.scala
+++ src/battingAnalysis/battingAverage/BattingAverage.scala
@@ -13,12 +13,16 @@ object BattingAverage {
     var IPLRdd = sc.textFile("/user/input/IPLMatchesBallByBall.csv", 5).map(_.split(","));
 
     //filter records from 2015 and innings 1 and 2 . 3rd and 4th innings comes under super over
+    //SEASON = 14 th column index  INNINGS=  columns index 1
     var IPLFilterRdd = IPLRdd.filter(record => record(14).toInt >= 2015 && record(1).toInt < 3);
 
     //calculating and filtering batsmen who has scored more than 300 runs
+    //STRIKER = 4th column index
+    //RUNS_IN_THAT_DELIEVRY = 7 th column index
     var batsmenRuns = IPLFilterRdd.map(record => (record(4), record(7).toDouble)).reduceByKey(_ + _).filter(_._2 > 300);
 
     //counting outs of all batsmen
+    //DISMISSED_PLAYER = 10 th  index column;
     var batsmenOuts = IPLFilterRdd.filter(_(10).nonEmpty).map(record => (record(10), 1)).reduceByKey(_ + _);
 
     var joinedData = batsmenRuns.join(batsmenOuts);
